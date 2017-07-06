f20ff423844e7d4e874278e7eef5b5afbe7aac04
Maximilian Senftleben <senftleben@cs.uni-kl.de>
2017-07-06 17:29:26 +0200

    Minimized code as suggested by LeaVerou

--- multirange.js
+++ multirange.js
@@ -10,18 +10,10 @@ self.multirange = function(input) {
 		return;
 	}
 
-	var values = [];
-	if(input.hasAttribute("value")) {
-		values = input.getAttribute("value").split(",");
-	}
-	var min = 0;
-	var max = 100;
-	if(input.hasAttribute("min")) {
-		min = +input.min;
-	}
-	if(input.hasAttribute("max")) {
-		max = +input.max;
-	}
+	var value = input.getAttribute("value");
+	var values = value === null ? [] : value.split(",");
+	var min = +(input.min || 0);
+	var max = +(input.max || 100);
 	var ghost = input.cloneNode();
 
 	input.classList.add("multirange", "original");
