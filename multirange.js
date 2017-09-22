f8aa8bf4a035f3e42a967c71eb07f8828a286eb2
John Slegers <slegersjohn@gmail.com>
2017-09-22 17:47:21 +0200

    This commit fixes three issues
    The following issues are fixed by this commit :
    * https://github.com/LeaVerou/multirange/issues/27
    * https://github.com/LeaVerou/multirange/issues/28
    * https://github.com/LeaVerou/multirange/issues/29

--- multirange.js
+++ multirange.js
@@ -1,82 +1,100 @@
 "use strict";
+var module = module || {};
 module.exports = (function(self) {
 
-	var supportsMultiple = self.HTMLInputElement && "valueLow" in HTMLInputElement.prototype;
+  var supportsMultiple = self.HTMLInputElement && "valueLow" in HTMLInputElement.prototype;
 
-	var descriptor = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, "value");
+  var descriptor = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, "value");
 
-	self.multirange = function(input) {
-	if (supportsMultiple || input.classList.contains("multirange")) {
-		return;
-	}
+  self.multirange = function(input) {
+    if (supportsMultiple || input.classList.contains("multirange")) {
+      return;
+    }
 
-	var value = input.getAttribute("value");
-	var values = value === null ? [] : value.split(",");
-	var min = +(input.min || 0);
-	var max = +(input.max || 100);
-	var ghost = input.cloneNode();
+    var value = input.getAttribute("value");
+    var values = value === null ? [] : value.split(",");
+    var min = +(input.min || 0);
+    var max = +(input.max || 100);
+    var ghost = input.cloneNode();
+    if (ghost.id) {
+      ghost.id += "Clone";
+    }
 
-	input.classList.add("multirange", "original");
-	ghost.classList.add("multirange", "ghost");
+    input.classList.add("multirange", "original");
+    ghost.classList.add("multirange", "ghost");
 
-	input.value = values[0] || min + (max - min) / 2;
-	ghost.value = values[1] || min + (max - min) / 2;
+    input.value = parseFloat(values[0]) || min + (max - min) / 2;
+    ghost.value = parseFloat(values[1]) || min + (max - min) / 2;
 
-	input.parentNode.insertBefore(ghost, input.nextSibling);
+    input.parentNode.insertBefore(ghost, input.nextSibling);
 
-	Object.defineProperty(input, "originalValue", descriptor.get ? descriptor : {
-		// Fuck you Safari >:(
-		get: function() { return this.value; },
-		set: function(v) { this.value = v; }
-	});
+    Object.defineProperty(input, "originalValue", descriptor.get ? descriptor : {
+      // Fuck you Safari >:(
+      get: function() {
+        return this.value;
+      },
+      set: function(v) {
+        this.value = v;
+      }
+    });
 
-	Object.defineProperties(input, {
-		valueLow: {
-			get: function() { return Math.min(this.originalValue, ghost.value); },
-			set: function(v) { this.originalValue = v; },
-			enumerable: true
-		},
-		valueHigh: {
-			get: function() { return Math.max(this.originalValue, ghost.value); },
-			set: function(v) { ghost.value = v; },
-			enumerable: true
-		}
-	});
+    Object.defineProperties(input, {
+      valueLow: {
+        get: function() {
+          return Math.min(this.originalValue, ghost.value);
+        },
+        set: function(v) {
+          this.originalValue = v;
+        },
+        enumerable: true
+      },
+      valueHigh: {
+        get: function() {
+          return Math.max(this.originalValue, ghost.value);
+        },
+        set: function(v) {
+          ghost.value = v;
+        },
+        enumerable: true
+      }
+    });
 
-	if (descriptor.get) {
-		// Again, fuck you Safari
-		Object.defineProperty(input, "value", {
-			get: function() { return this.valueLow + "," + this.valueHigh; },
-			set: function(v) {
-				var values = v.split(",");
-				this.valueLow = values[0];
-				this.valueHigh = values[1];
-				update();
-			},
-			enumerable: true
-		});
-	}
+    if (descriptor.get) {
+      // Again, fuck you Safari
+      Object.defineProperty(input, "value", {
+        get: function() {
+          return this.valueLow + "," + this.valueHigh;
+        },
+        set: function(v) {
+          var values = v.split(",");
+          this.valueLow = values[0];
+          this.valueHigh = values[1];
+          update();
+        },
+        enumerable: true
+      });
+    }
 
-	function update() {
-		ghost.style.setProperty("--low", 100 * ((input.valueLow - min) / (max - min)) + 1 + "%");
-		ghost.style.setProperty("--high", 100 * ((input.valueHigh - min) / (max - min)) - 1 + "%");
-	}
+    function update() {
+      ghost.style.setProperty("--low", 100 * ((input.valueLow - min) / (max - min)) + 1 + "%");
+      ghost.style.setProperty("--high", 100 * ((input.valueHigh - min) / (max - min)) - 1 + "%");
+    }
 
-	input.addEventListener("input", update);
-	ghost.addEventListener("input", update);
+    input.addEventListener("input", update);
+    ghost.addEventListener("input", update);
 
-	update();
-	};
+    update();
+  };
 
   self.multirange.init = function() {
-		[].slice.call(document.querySelectorAll("input[type=range][multiple]:not(.multirange)")).forEach(self.multirange);
-	};
+    [].slice.call(document.querySelectorAll("input[type=range][multiple]:not(.multirange)")).forEach(self.multirange);
+  };
 
-	if (document.readyState === "loading") {
-	document.addEventListener("DOMContentLoaded", self.multirange.init);
-	} else {
-	self.multirange.init();
-	}
-	return self.multirange;
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", self.multirange.init);
+  } else {
+    self.multirange.init();
+  }
+  return self.multirange;
 
-})(typeof window === 'undefined'? (typeof global === 'undefined' ? {} : global) : window);
+})(typeof window === 'undefined' ? (typeof global === 'undefined' ? {} : global) : window);
